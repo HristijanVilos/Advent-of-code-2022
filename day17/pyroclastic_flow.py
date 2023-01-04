ee665f4d1fdc84457edbe0e43a4f37f158ce0ea8
Hristijan Vilos <hristijan.vilos@protonmail.com>
2023-01-04 22:33:42 +0100

    Day 17: Pyroclastic Flow

--- day17/pyroclastic_flow.py
+++ day17/pyroclastic_flow.py
@@ -50,10 +50,10 @@ def can_move_down(cur_pos: list, grid: set):
     return True
 
 
-def calc_cur_pos(pos: int, shape: tuple) -> list:
+def starting_pos(pos: int, shape: tuple) -> list:
     res = []
     for s in shape:
-        res.append([s[0] + pos, s[1] +2])
+        res.append([s[0] + pos, s[1] + 2])
     return res
 
 
@@ -76,56 +76,62 @@ def parse() -> str:
         return file.read()
 
 
-def part_1():
+def signature(maxX, grid):
+    # maxX-x<= save the last number of rows
+    # 4 is the lowest number of last rows of signiture that can prove
+    # the pattern for my case (found iteratively)
+    return frozenset([(maxX - x, y) for (x, y) in grid if maxX - x <= 4])
+
+
+def update_grid(grid: set, dh: int, times_repeating: int):
+    new_grid = set()
+    for (x, y) in grid:
+        new_grid.add((x+(dh*times_repeating), y))
+    return new_grid
+
+
+def solution(H: int):
     grid = set([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6)])
     directions = parse()
     len_dir = len(directions)
     count = 0
+    move = 0
     max_height = 0
-    for move in range(2022):
-        shape = SHAPES[move%5]
+    paterns_seen = {}
+    once = True
+    while move < H:
+        shape = SHAPES[move % 5]
         can_proceed = True
         pos = max_height + 4
-        cur_pos = calc_cur_pos(pos, shape)
+        cur_pos = starting_pos(pos, shape)
         while can_proceed:
-            get_dir = count % len_dir
-            _dir = directions[get_dir]
+            dir_num = count % len_dir
+            _dir = directions[dir_num]
             count += 1
             DIR.get(_dir)(cur_pos, grid)
             if can_move_down(cur_pos, grid):
                 move_down(cur_pos)
             else:
                 max_height = setled_rock(cur_pos, grid, max_height)
+                # the place in the direction pattern, the shape,
+                # signature of the last rows (in my case 4 is enough 🤯)
+                SR = (dir_num, move % 5, signature(max_height, grid))
                 can_proceed = False
 
+                if SR in paterns_seen and once:
+                    once = False
+                    (old_move, old_height) = paterns_seen[SR]
+                    dm = move - old_move
+                    dh = max_height - old_height
+                    times_repeating = (H - move) // dm
+                    max_height += times_repeating * dh
+                    move += times_repeating * dm
+                    grid = update_grid(grid, dh, times_repeating)
+                    break
+                paterns_seen[SR] = (move, max_height)
+        move += 1
     return max_height
 
 
-def part_2():
-    grid = set([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6)])
-    directions = parse()
-    len_dir = len(directions)
-    count = 0
-    max_height = 0
-    for move in range(1_000_000_000_000):
-        shape = SHAPES[move%5]
-        can_proceed = True
-        pos = max_height + 4
-        cur_pos = calc_cur_pos(pos, shape)
-        
-        while can_proceed:
-            get_dir = count % len_dir
-            _dir = directions[get_dir]
-            count += 1
-            DIR.get(_dir)(cur_pos, grid)
-            if can_move_down(cur_pos, grid):
-                move_down(cur_pos)
-            else:
-                max_height = setled_rock(cur_pos, grid, max_height)
-                can_proceed = False
-
-    return max_height
-
-
-print("Part 1:", part_1())
-# print("Part 2:", part_2())
+print("Part 1:", solution(H=2022))
+print("Part 2:", solution(H=1_000_000_000_000))
